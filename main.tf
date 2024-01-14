0722b43b8aa146132c2fc8503a4c9503117a2334
Craig Thacker <craigthackerx@gmail.com>
2024-01-14 00:14:01 +0000

    Update code

--- main.tf
+++ main.tf
@@ -1,1 +1,35 @@
-#
+resource "azurerm_databricks_workspace" "this" {
+    for_each = { for k, v in var.databricks_workspaces : k => v }
+
+
+  name                = each.value.name
+  resource_group_name = var.rg_name
+  location            = var.location
+  tags                = var.tags
+  sku                 = lower(each.value.sku)
+
+  load_balancer_backend_address_pool_id = each.value.load_balancer_backend_address_pool_id
+  managed_services_cmk_key_vault_key_id = each.value.managed_services_cmk_key_vault_key_id
+  managed_disk_cmk_key_vault_key_id = each.value.managed_disk_cmk_key_vault_key_id
+  managed_disk_cmk_rotation_to_latest_version_enabled = each.value.managed_disk_cmk_rotation_to_latest_version_enabled
+  customer_managed_key_enabled = each.value.customer_managed_key_enabled
+  infrastructure_encryption_enabled = each.value.infrastructure_encryption_enabled
+  public_network_access_enabled = each.value.public_network_access_enabled
+
+  dynamic "custom_parameters" {
+    for_each = each.value.custom_parameters != null ? each.value.custom_parameters : []
+    content {
+      machine_learning_workspace_id = custom_parameters.value.machine_learning_workspace_id
+      nat_gateway_name = custom_parameters.value.nat_gateway_name
+      public_ip_name = custom_parameters.value.public_ip_name
+      no_public_ip = custom_parameters.value.no_public_ip
+      public_subnet_name = custom_parameters.value.public_subnet_name
+      public_subnet_network_security_group_association_id = custom_parameters.value.public_subnet_network_security_group_association_id
+      private_subnet_name = custom_parameters.value.private_subnet_name]
+      private_subnet_network_security_group_association_id = custom_parameters.value.private_subnet_network_security_group_association_id
+      storage_account_name = custom_parameters.value.storage_account_name
+      storage_account_sku_name = custom_parameters.value.storage_account_sku_name
+      virtual_network_id = custom_parameters.value.virtual_network_id
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,44 @@
-#
+variable "databricks_workspaces" {
+  description = "The databricks workspaces to create"
+  type = list(object({
+    name = string
+    sku = string
+    load_balancer_backend_address_pool_id = optional(string)
+    managed_services_cmk_key_vault_key_id = optional(string)
+    managed_disk_cmk_key_vault_key_id = optional(string)
+    managed_disk_cmk_rotation_to_latest_version_enabled = optional(bool)
+    customer_managed_key_enabled = optional(bool)
+    infrastructure_encryption_enabled = optional(bool)
+    public_network_access_enabled = optional(bool, false)
+    custom_parameters = optional(list(object({
+      machine_learning_workspace_id = optional(string)
+      nat_gateway_name = optional(string, "nat-gateway")
+      public_ip_name = optional(string, "nat-gw-public-ip")
+      no_public_ip = optional(bool)
+      public_subnet_name = optional(string)
+      public_subnet_network_security_group_association_id = optional(string)
+      private_subnet_name = optional(string)
+      private_subnet_network_security_group_association_id = optional(string)
+      storage_account_name = optional(string)
+      storage_account_sku_name = optional(string)
+      virtual_network_id = optional(string)
+      vnet_address_prefix = optional(string, "10.139")
+
+    }))
+  }))
+}
+
+variable "location" {
+  description = "The location for this resource to be put in"
+  type        = string
+}
+
+variable "rg_name" {
+  description = "The name of the resource group, this module does not create a resource group, it is expecting the value of a resource group already exists"
+  type        = string
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A map of the tags to use on the resources that are deployed with this module."
+}
